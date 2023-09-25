295bae269be6aec4cfb2979f1d567930fe57d911
Abhay Singh <1995.abhaysingh@gmail.com>
2023-09-25 23:53:04 +0530

    first api created

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Import required modules and packages
-const express = require('express');
+const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 const port = process.env.PORT || 3000; // Set the port
 
@@ -7,11 +8,43 @@ const port = process.env.PORT || 3000; // Set the port
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Define routes and middleware
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+// Create a connection to the MySQL database
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "baatein",
 });
 
+// Define routes and middleware
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const login = (callback, data) => {
+  db.query(`select * from users where user_email = "${data?.email}"`, function (err, results, fields) {
+    try {
+      if (err) {
+        callback({ messge: "Internal server error!", code: 500 });
+        return;
+      }
+      if (results.length === 0) {
+        callback({ message: "User not found!", code: 401 });
+        return
+      }
+      callback({ message: data, code: 200 });
+    } catch (error) {
+      callback({message: error, code: 500})
+      return
+    }
+  });
+};
+
+app.post("/login", (req, res) => {
+  login((data) => {
+    const {message, code} = data
+    res.status(code).send(message);
+  }, req?.body);
+});
 
 // Start the server
 app.listen(port, () => {
