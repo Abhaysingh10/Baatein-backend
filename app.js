1c2972365853b63fdb08da33af82be9a19320cef
Abhay Singh <1995.abhaysingh@gmail.com>
2023-11-05 18:43:06 +0530

    socket setup completed

--- app.js
+++ app.js
@@ -2,18 +2,44 @@
 const express = require("express");
 const mysql = require("mysql2");
 const app = express();
-const port = process.env.PORT || 3000; // Set the port
+const { colours } = require("nodemon/lib/config/defaults");
+const port = 3000; // Set the port
+var cors = require('cors')
+// var http = require("http");
+// var server = http.createServer(app);
+// var io = require("socket.io")(server, {
+//   cors:{
+//     origin:"http:localhost:3001",
+//     credentials:false
+//   }
+// });
+
+const { Server } = require("socket.io");
+const {createServer }= require('http');
+var server = createServer(app);
+const io =new Server(server,  {
+  cors: {
+    origin: "http://localhost:3001",
+    credentials:false
+  }
+});
 
 // Middleware for parsing JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors())
 // Create a connection to the MySQL database
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  database: "baatein",
+  password:"root",
+  database: "baatein"
 });
+// const io = socketIo(server)
+
+io.sockets.on('connection', (socket)=>{
+  console.log('a user connected', socket.id)
+})
 
 // Define routes and middleware
 app.get("/", (req, res) => {
@@ -46,7 +72,27 @@ app.post("/login", (req, res) => {
   }, req?.body);
 });
 
+const getAllUsers = (callback)=>{
+  db.query(`select * from users where first_name = 'willy'`, (err, result)=>{
+    if (err) {
+      
+    }
+    callback( { status:200, data: result})
+  })
+}
+
+app.get ("/get-all-users", (req, res)=>{
+  getAllUsers((data)=>{
+    res.send(data)
+  })
+})
+
 // Start the server
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  db.connect(function(err){
+    if (err) throw err
+    console.log("db connected");
+  })
+  
 });
---
+++ database.js
@@ -0,0 +1,11 @@
+
+
+
+// Create a connection to the MySQL database
+const db = mysql.createConnection({
+    host: "localhost",
+    database: "baatein",
+    user: "root",
+    password:"root",
+  });
+  
